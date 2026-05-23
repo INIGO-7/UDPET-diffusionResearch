c72d5df414d84796f9eafb687ef3781dcc57e0b1
INIGO-7 <inigofernandezsopena@gmail.com>
2026-05-23 19:15:46 +0200

    Updated config with macbook M4 PRO constraints

--- src/butterfly_generation/config.py
+++ src/butterfly_generation/config.py
@@ -3,10 +3,10 @@ from dataclasses import dataclass
 @dataclass
 class TrainingConfig:
     image_size = 512  # the generated image resolution
-    train_batch_size = 4
+    train_batch_size = 1
     eval_batch_size = 4  # how many images to sample during evaluation
     num_epochs = 50
-    gradient_accumulation_steps = 1
+    gradient_accumulation_steps = 4
     learning_rate = 1e-4
     lr_warmup_steps = 500
     save_image_epochs = 10
